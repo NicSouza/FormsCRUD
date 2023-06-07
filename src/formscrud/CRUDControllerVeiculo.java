247c13605bbc9dcf9e1c4ab1bd419ff619fa8e32
Nicole Souza <nicolemadeleines@gmail.com>
2023-06-06 21:53:59 -0300

    Att

--- src/formscrud/CRUDControllerVeiculo.java
+++ src/formscrud/CRUDControllerVeiculo.java
@@ -1,5 +1,6 @@
 package formscrud;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,8 +13,13 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +30,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
@@ -335,7 +342,15 @@ public class CRUDControllerVeiculo implements Initializable{
         veiculo_Cor.setText(String.valueOf(vData.getCor()));
 
     }
-
+    
+    public void changeToCrudFormLugar(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("crudFormLugar.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
